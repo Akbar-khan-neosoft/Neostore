0201f2f270c0390710ed4a9756b71ed05b19a35d
neoketan <akbar.khan@neosofttech.com>
2020-03-12 17:04:32 +0530

    carousel implementation done using redux and contact form implemented along with routing

--- src/Components/Pages/Dashboard/Dashboard_Slider/Dashboard_Slider.js
+++ src/Components/Pages/Dashboard/Dashboard_Slider/Dashboard_Slider.js
@@ -13,8 +13,6 @@ class DashboardCarousel extends Component {
 
 	componentDidMount() {
 		this.props.onFetch();
-
-		// this.setState({ data: result.data.category_details });
 	}
 
 	render() {
@@ -22,11 +20,8 @@ class DashboardCarousel extends Component {
 			<div className="dashboard-slider">
 				<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 					<div class="carousel-inner">
-						{console.log('dashboard : ', this.props.data)}
-						{[this.props.data].map((item, index) => (
+						{this.props.data.map((item, index) => (
 							<div class={`carousel-item ${index == 0 ? 'active' : ''}`} key={index}>
-								{console.log('inside : ', item.sliderReducer.data.data)}
-
 								<img
 									src={'http://180.149.241.208:3022/' + item.product_image}
 									width="100%"
@@ -51,10 +46,7 @@ class DashboardCarousel extends Component {
 }
 
 const mapStateToProps = state => {
-	{
-		console.log('redu:', state);
-	}
-	return { data: state };
+	return { data: state.sliderReducer.data.category_details || [] };
 };
 
 const mapDispatchToProps = dispatch => ({
--- src/Redux/Actions/sliderAction.js
+++ src/Redux/Actions/sliderAction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { SLIDER_DATA_FAILURE, SLIDER_DATA_REQUEST, SLIDER_DATA_SUCCESS } from '../Constants';
 
 const sliderDataRequest = () => ({ type: SLIDER_DATA_REQUEST });
-const sliderDataSuccess = data => ({ type: SLIDER_DATA_SUCCESS, payload: { data } });
+const sliderDataSuccess = data => ({ type: SLIDER_DATA_SUCCESS, payload: data });
 const sliderDataFailure = error => ({ type: SLIDER_DATA_FAILURE, error: error });
 
 export const fetchSliderData = () => async dispatch => {
@@ -10,13 +10,9 @@ export const fetchSliderData = () => async dispatch => {
 
 	dispatch(sliderDataRequest());
 	try {
-		const { data } = await axios.get('http://180.149.241.208:3022/getAllCategories').then(result => {
-			return result;
+		await axios.get('http://180.149.241.208:3022/getAllCategories').then(result => {
+			return dispatch(sliderDataSuccess(result.data));
 		});
-		{
-			console.log('in try ' + data);
-		}
-		dispatch(sliderDataSuccess(data));
 	} catch (error) {
 		dispatch(sliderDataFailure({ error }));
 	}
