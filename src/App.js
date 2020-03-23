e4557015473dbec8ab109447c46151e9b961861f
akbar-khan-neosoft <akbar.khan@neosofttech.com>
2020-03-23 12:02:39 +0530

    all routing has been moved to seperate routes file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Dashboard from './Components/Pages/Dashboard/Dashboard';
-import Login from './Components/Pages/Login';
+// import Dashboard from './Components/Pages/Dashboard/Dashboard';
+// import Login from './Components/Pages/Login';
 import Layout from './Components/Main/Layout';
-import Register from './Components/Pages/Register';
+// import Register from './Components/Pages/Register';
 import store from '../src/Redux/Store';
-import ContactForm from './Components/Common/ContactForm';
-import ForgotPassword from './Components/Common/ForgotPassword';
-import Product from './Components/Pages/Dashboard/Product';
-import RecoverPassword from './Components/Pages/RecoverPassword';
-import ProductDetails from './Components/Pages/ProductDetails';
-import Profile from './Components/Pages/Profile';
-import Loading from './Components/Common/Loading';
-import AddNewAddress from './Components/Pages/AddNewAddress';
+import Routes from './NeoStore_Router/Routes';
+// import ContactForm from './Components/Common/ContactForm';
+// import ForgotPassword from './Components/Common/ForgotPassword';
+// import Product from './Components/Pages/Dashboard/Product';
+// import RecoverPassword from './Components/Pages/RecoverPassword';
+// import ProductDetails from './Components/Pages/ProductDetails';
+// import Profile from './Components/Pages/Profile';
+// import Loading from './Components/Common/Loading';
+// import AddNewAddress from './Components/Pages/AddNewAddress';
 // console.log(store.getState());
 
 function App() {
@@ -22,7 +23,8 @@ function App() {
 			<Provider store={store}>
 				<Router>
 					<Layout>
-						<Switch>
+						<Routes/>
+						{/* <Switch>
 							<Route exact path="/" component={Dashboard} />
 							<Route exact path="/login" component={Login} />
 							<Route exact path="/register" component={Register} />
@@ -33,12 +35,7 @@ function App() {
 							<Route excat path="/productdetails" component={ProductDetails} />
 							<Route excat path="/myaccount" component={Profile} />
 							<Route excat path="/address" component={AddNewAddress} />
-
-							
-
-
-
-						</Switch>
+						</Switch> */}
 					</Layout>
 					
 				</Router>
---
+++ src/NeoStore_Router/Routes.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Route, Switch } from 'react-router-dom';
+import Dashboard from '../Components/Pages/Dashboard/Dashboard';
+import Login from '../Components/Pages/Login';
+import Register from '../Components/Pages/Register';
+import ContactForm from '../Components/Common/ContactForm';
+import ForgotPassword from '../Components/Common/ForgotPassword';
+import Product from '../Components/Pages/Dashboard/Product';
+import RecoverPassword from '../Components/Pages/RecoverPassword';
+import ProductDetails from '../Components/Pages/ProductDetails';
+import Profile from '../Components/Pages/Profile';
+import AddNewAddress from '../Components/Pages/AddNewAddress';
+
+function Routes(){
+    return(
+
+					
+                    <Switch>
+                        <Route exact path="/" component={Dashboard} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/contactus" component={ContactForm} />
+                        <Route excat path="/forgotpassword" component={ForgotPassword} />
+                        <Route excat path="/product" component={Product} />
+                        <Route excat path="/recoverpassword" component={RecoverPassword} />
+                        <Route excat path="/productdetails" component={ProductDetails} />
+                        <Route excat path="/myaccount" component={Profile} />
+                        <Route excat path="/address" component={AddNewAddress} />
+                    </Switch>
+ 
+    )
+}
+
+export default Routes
+
